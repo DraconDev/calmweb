55ec33c6d01e1fe48d54f3b632151ba602fd21a3
DraconDev <dracsharp@gmail.com>
2026-03-11 17:15:57 +0000

    [[daemon] sync: 1 file(s)|wip] 1 modified apps/extension/entrypoints/popup/App.tsx

--- apps/extension/entrypoints/popup/App.tsx
+++ apps/extension/entrypoints/popup/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { sendToBackground, getActiveTab } from '@dracon/wxt-shared/extension';
 import { MESSAGE_TYPES } from '@/src/messaging';
 import { youtubeAdapter, redditAdapter, xAdapter } from '@/src/adapters';
