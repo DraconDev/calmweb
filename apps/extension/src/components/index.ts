74e944d5ab8c368975b1c89c1a4988fc4223a88b
DraconDev <dracsharp@gmail.com>
2026-03-12 03:59:40 +0000

    [[daemon] sync: 1 file(s)|wip] 1 added apps/extension/src/components/index.ts

---
+++ apps/extension/src/components/index.ts
@@ -0,0 +1,10 @@
+// Layout components
+export { Container } from './layout/Container';
+export { Header, Footer, PageLayout } from './layout/HeaderFooter';
+export { Card } from './layout/Card';
+export { FormField, FormRow, FormActions } from './layout/FormLayout';
+export { Tabs, TabList, TabTrigger, TabContent } from './layout/Tabs';
+
+// UI components
+export { Switch } from './ui/Switch';
+export { Spinner } from './ui/Spinner';
