9364fce0058de0b1071c80990bced92e771e4eb0
DraconDev <dracsharp@gmail.com>
2026-03-13 06:42:47 +0000

    [[daemon] sync: 1 file(s)|wip] 1 added apps/extension/entrypoints/reader.ts

---
+++ apps/extension/entrypoints/reader.ts
@@ -0,0 +1,26 @@
+/**
+ * Reader Entry Point for CalmWeb
+ *
+ * Listens for keyboard shortcut to toggle Super Reader mode.
+ */
+
+export default defineContentScript({
+  matches: ['<all_urls>'],
+
+  main() {
+    import('../src/renderer/reader').then(({ toggleReader }) => {
+      document.addEventListener('keydown', (e) => {
+        if (e.altKey && e.key.toLowerCase() === 'r') {
+          e.preventDefault();
+          toggleReader();
+        }
+      });
+
+      browser.runtime.onMessage.addListener((message) => {
+        if (message.type === 'TOGGLE_READER') {
+          toggleReader();
+        }
+      });
+    });
+  },
+});
