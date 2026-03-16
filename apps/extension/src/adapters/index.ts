d4e9e00402041f9edc983a6515ba9cd1f3cd4f7f
DraconDev <dracsharp@gmail.com>
2026-03-16 17:11:38 +0000

    chore(misc): wip checkpoint --- category: chore scope: misc ---

--- apps/extension/src/adapters/index.ts
+++ apps/extension/src/adapters/index.ts
@@ -2,3 +2,4 @@ export { youtubeAdapter } from './youtube';
 export { redditAdapter } from './reddit';
 export { xAdapter } from './x';
 export { universalAdapter } from './universal';
+export { isGoogleSearchPage, extractSearchData } from './google';
